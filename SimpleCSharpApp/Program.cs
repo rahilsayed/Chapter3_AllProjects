2333bcf0ae08580071987ef8ad287227171d9080
Rahil Sayed <rahilsayed373@gmail.com>
2023-12-19 18:20:11 +0530

    Added code in SimpleCSharpApp project.

--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Display a simple message to the user.
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+
+//Process any incoming args.
+//foreach(string arg in args)
+//{
+//    Console.WriteLine("Args: {0}", arg);
+//}
+// Get arguments using System.Environment.
+string[] theArgs = Environment.GetCommandLineArgs();foreach (string arg in theArgs)
+{
+    Console.WriteLine("Arg: {0}", arg);
+}
+
+// Local method within the Top-level statements.
+ShowEnvironmentDetails();
+
+
+// Wait for Enter key to be pressed before shutting down.
+Console.ReadLine();
+
+//Return an arbitrary error code.
+return -1;
+
+//Additional Members of the System.Environment Class demo
+static void ShowEnvironmentDetails(){
+    // Print out the drives on this machine,
+    // and other interesting details.
+    foreach (string drive in Environment.GetLogicalDrives())
+    {
+        Console.WriteLine("Drive: {0}", drive);
+    }
+    Console.WriteLine("OS: {0}", Environment.OSVersion);
+    Console.WriteLine("Number of processors: {0}",
+    Environment.ProcessorCount);
+    Console.WriteLine(".NET Core Version: {0}",
+    Environment.Version);    Console.WriteLine("Machine Name: {0}", Environment.MachineName);}
+
