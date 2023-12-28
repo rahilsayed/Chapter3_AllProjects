87cdd8e6b9fc6dfe3d4cd58f5aab2168f5a5bd82
Rahil Sayed <rahilsayed373@gmail.com>
2023-12-28 16:49:40 +0530

    updated code in FunWithMethodOverloading proj

---
+++ FunWithMethodOverloading/AddOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithMethodOverloading;
+// C# code.
+// Overloaded Add() method.
+public static class AddOperations
+{
+    // Overloaded Add() method.
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+    public static long Add(long x, long y)
+    {
+        return x + y;
+    }
+}
