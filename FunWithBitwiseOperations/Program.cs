215221f78a4f46188cc07d8549ccbe49e2f176f2
Rahil Sayed <83951702+rahilsayed@users.noreply.github.com>
2023-12-28 22:05:34 +0530

    merge resolving from working to main (#16)
    * added new code in FunWithMethods proj
    
    * added new proj FunWithMethodOverloading
    
    * added FunWithEnums prog
    
    * added FunWithBitwiseOperations prog
    
    * added FunWithBitwiseOperations prog

---
+++ FunWithBitwiseOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿using FunWithBitwiseOperations;
+Console.WriteLine("===== Fun wih Bitwise Operations");
+Console.WriteLine("6 & 4 = {0} | {1}", 6 & 4, Convert.ToString((6 & 4), 2));
+Console.WriteLine("6 | 4 = {0} | {1}", 6 | 4, Convert.ToString((6 | 4), 2));
+Console.WriteLine("6 ^ 4 = {0} | {1}", 6 ^ 4, Convert.ToString((6 ^ 4), 2));
+Console.WriteLine("6 << 1 = {0} | {1}", 6 << 1, Convert.ToString((6 << 1), 2));
+Console.WriteLine("6 >> 1 = {0} | {1}", 6 >> 1, Convert.ToString((6 >> 1), 2));
+Console.WriteLine("~6 = {0} | {1}", ~6, Convert.ToString(~((short)6), 2));
+Console.WriteLine("Int.MaxValue {0}", Convert.ToString((int.MaxValue), 2));
+
+
+ContactPreferenceEnum emailAndPhone = ContactPreferenceEnum.Email | ContactPreferenceEnum.Phone;
+
+Console.WriteLine("None? {0}", (emailAndPhone | ContactPreferenceEnum.None) == emailAndPhone);
+Console.WriteLine("Email? {0}", (emailAndPhone | ContactPreferenceEnum.Email) == emailAndPhone);
+Console.WriteLine("Phone? {0}", (emailAndPhone | ContactPreferenceEnum.Phone) == emailAndPhone);
+Console.WriteLine("Text? {0}", (emailAndPhone | ContactPreferenceEnum.Text) == emailAndPhone);
+
+Console.ReadLine();
